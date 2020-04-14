fb63027af302f0eae343c0296fad95e0b4b1a4fb
Edward Nash <e.nash@dvz-mv.de>
2020-04-14 11:33:40 +0200

    Update plugin metadata (author, version)

--- wfsclient.py
+++ wfsclient.py
@@ -80,7 +80,20 @@ class WfsClient:
             self.iface.removePluginMenu("&WFS 2.0 Client", self.aboutAction)
 
     def about(self):
-        infoString = "<table><tr><td colspan=\"2\"><b>WFS 2.0 Client 0.9.7 beta</b></td></tr><tr><td colspan=\"2\"></td></tr><tr><td>Author:</td><td>J&uuml;rgen Weichand</td></tr><tr><td>Mail:</td><td><a href=\"mailto:juergen@weichand.de\">juergen@weichand.de</a></td></tr><tr><td>Website:</td><td><a href=\"http://www.weichand.de\">http://www.weichand.de</a></td></tr></table>"
+        infoString = "<table>" \
+                     "<tr><td colspan=\"2\"><b>WFS 2.0 Client 0.9.8 beta</b></td></tr>" \
+                     "<tr><td colspan=\"2\"></td></tr>" \
+                     "<tr><td rowspan=\"3\">Authors:</td>" \
+                     "<td>J&uuml;rgen Weichand " \
+                     "(<a href=\"mailto:juergen@weichand.de\">juergen@weichand.de</a>)</td></tr>" \
+                     "<tr><td>Tim Vinzing</td></tr>" \
+                     "<tr><td>Edward Nash " \
+                     "(<a href=\"mailto:e.nash@dvz-mv.de\">e.nash@dvz-mv.de</a>)</td></tr>" \
+                     "<tr><td colspan=\"2\"></td></tr>" \
+                     "<tr><td>Website:</td>" \
+                     "<td><a href=\"https://github.com/qgisinspiretools/qgis-wfs20-client-plugin\">" \
+                     "https://github.com/qgisinspiretools/qgis-wfs20-client-plugin</a></td></tr>" \
+                     "</table>"
         QMessageBox.information(self.iface.mainWindow(), "About WFS 2.0 Client", infoString)
 
     # run method that performs all the real work
